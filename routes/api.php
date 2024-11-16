05eb3eb9292071f7a836fa7e9f3460411e879279
Rakhaditowb <rakhaditobassam@gmail.com>
2024-11-16 19:16:03 +0700

    API: Update route '/' for logout handling

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,16 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/user', [UserController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/user/{id}', [UserController::class, 'show'])->middleware('auth:sanctum');
 
+
+Route::get('/' , function (){
+    return response()->json([
+        'success' => false,
+        'response_code' => 401,
+        'message' => 'Unauthenticated. Token not provided or invalid.',
+        'data' => []
+    ], 401);
+})->name('login');
+
 //auth
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
